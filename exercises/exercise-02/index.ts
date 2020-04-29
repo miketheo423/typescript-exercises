9182c06d1f6331585eb0f841d2cb855ef87f388e
miketheo423 <miketheo423@gmail.com>
2020-04-29 15:44:23 -0600

    solves challenge 2

--- exercises/exercise-02/index.ts
+++ exercises/exercise-02/index.ts
@@ -27,50 +27,54 @@ Run:
 */
 
 interface User {
-    name: string;
-    age: number;
-    occupation: string;
+  name: string;
+  age: number;
+  occupation: string;
 }
 
 interface Admin {
-    name: string;
-    age: number;
-    role: string;
+  name: string;
+  age: number;
+  role: string;
 }
 
 type Person = User | Admin;
 
 const persons: Person[] = [
-    {
-        name: 'Max Mustermann',
-        age: 25,
-        occupation: 'Chimney sweep'
-    },
-    {
-        name: 'Jane Doe',
-        age: 32,
-        role: 'Administrator'
-    },
-    {
-        name: 'Kate Müller',
-        age: 23,
-        occupation: 'Astronaut'
-    },
-    {
-        name: 'Bruce Willis',
-        age: 64,
-        role: 'World saver'
-    }
+  {
+    name: 'Max Mustermann',
+    age: 25,
+    occupation: 'Chimney sweep',
+  },
+  {
+    name: 'Jane Doe',
+    age: 32,
+    role: 'Administrator',
+  },
+  {
+    name: 'Kate Müller',
+    age: 23,
+    occupation: 'Astronaut',
+  },
+  {
+    name: 'Bruce Willis',
+    age: 64,
+    role: 'World saver',
+  },
 ];
 
 function logPerson(person: Person) {
-    let additionalInformation: string;
-    if (person.role) {
-        additionalInformation = person.role;
-    } else {
-        additionalInformation = person.occupation;
-    }
-    console.log(` - ${chalk.green(person.name)}, ${person.age}, ${additionalInformation}`);
+  let additionalInformation: string;
+  const admin = person as Admin;
+  const user = person as User;
+  if (admin) {
+    additionalInformation = admin.role;
+  } else {
+    additionalInformation = user.occupation;
+  }
+  console.log(
+    ` - ${chalk.green(person.name)}, ${person.age}, ${additionalInformation}`
+  );
 }
 
 persons.forEach(logPerson);
